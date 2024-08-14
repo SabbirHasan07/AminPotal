e7a6ba5c8168e8154b875477ca8719d1f9a19f5c
SabbirHasan07 <sabbirhasan.engr@gmail.com>
2024-08-14 15:21:16 +0600

    Add project files.

---
+++ AminPotal/Data/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using AminPotal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AminPotal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationDBContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
